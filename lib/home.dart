2d94fcb303333d86a9c79a9fb51585ab9212aca2
Laksh <lakshyajeet22@gmail.com>
2019-02-01 22:41:01 +0800

    Make homepage

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tech_fest_app/auth/auth.dart';
+import 'package:tech_fest_app/common/primary_button.dart';
 
 class Home extends StatelessWidget {
   Home({this.auth, this.onSignOut});
@@ -21,6 +22,7 @@ class Home extends StatelessWidget {
 
     return new Scaffold(
         appBar: new AppBar(
+          title: new Text("Home"),
           actions: <Widget>[
             new FlatButton(
                 onPressed: _signOut,
@@ -29,11 +31,64 @@ class Home extends StatelessWidget {
           ],
         ),
         body: new Center(
-          child: new Text(
-            'Welcome',
-            style: new TextStyle(fontSize: 32.0),
-          ),
+          child:new SingleChildScrollView(child: new Container(
+              padding: const EdgeInsets.all(16.0),
+              child: new Column(
+                  children: [
+                    new Image.asset(
+                        'img/ntu.png',
+                    height: 250.0,
+                    width: 250.0),
+                    new Text(
+                      'Welcome to NTU',
+                      style: new TextStyle(fontSize: 32.0),
+                    ),
+                    new Card(
+                        child: new Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              new Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: new PrimaryButton(
+                                    key: null,
+                                    onPressed: null,
+                                    text: 'Classes',
+                                    height: 44.0,
+                                  )
+                              ),
+                              new Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: new PrimaryButton(
+                                    key: null,
+                                    onPressed: null,
+                                    text: 'Events',
+                                    height: 44.0,
+                                  )
+                              ),
+                              new Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: new PrimaryButton(
+                                    key: null,
+                                    onPressed: null,
+                                    text: 'Report a fault',
+                                    height: 44.0,
+                                  )
+                              ),
+                              new Container(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: new PrimaryButton(
+                                    key: null,
+                                    onPressed: null,
+                                    text: 'Get some food',
+                                    height: 44.0,
+                                  )
+                              ),
+                            ])
+                    ),
+                  ]
+              )
+          )
         )
-    );
+    ));
   }
 }
