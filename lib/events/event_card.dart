648900e6071c314f2db16e61ffa42c6e60089bfb
Laksh <lakshyajeet22@gmail.com>
2019-02-02 15:10:33 +0800

    Add events popup

--- lib/events/event_card.dart
+++ lib/events/event_card.dart
@@ -4,18 +4,105 @@ import 'package:tech_fest_app/common/primary_button.dart';
 class EventsCard extends StatefulWidget {
   @override
   _EventsCardState createState() => _EventsCardState();
-  String eventName;
-  EventsCard({this.eventName});
+  String eventName,
+      eventHolder,
+      eventDate,
+      eventDescription,
+      eventImage,
+      eventPrice,
+      eventLocation;
+  EventsCard(
+      {this.eventName,
+      this.eventHolder,
+      this.eventDate,
+      this.eventDescription,
+      this.eventImage,
+      this.eventPrice,
+      this.eventLocation});
 }
 
 class _EventsCardState extends State<EventsCard> {
+  void _showDialog() {
+    showDialog(
+        context: context,
+        child: new AlertDialog(
+          content: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  widget.eventName,
+                  style:
+                      new TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.date_range),
+                    new Text(widget.eventDate)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.location_on),
+                    new Text(widget.eventLocation)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  children: <Widget>[
+                    new Icon(Icons.attach_money),
+                    new Text(widget.eventPrice)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  "This will be a small description of the thing so that people know what will happen",
+                  overflow: TextOverflow.clip,
+                  maxLines: 2,
+                  style: new TextStyle(fontSize: 18.0, color: Colors.black),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: new Align(
+                    alignment: Alignment.center,
+                    child: new RaisedButton(
+                        child: new Text("Register",
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 20.0)),
+                        shape: new RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0 / 2))),
+                        color: Colors.blue,
+                        textColor: Colors.black87,
+                        onPressed: () {
+                          print("This");
+                        }),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
 
-
-
-  //TODO: Make fields as props
-  //TODO: Solve text field overflow problem
   @override
   Widget build(BuildContext context) {
+    //TODO: Make fields as props
+    //TODO: Solve text field overflow problem
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: new Card(
@@ -34,7 +121,8 @@ class _EventsCardState extends State<EventsCard> {
                   child: Container(
                       child: new Text(
                     "18/2/19",
-                    style: new TextStyle(fontSize: 22.0, color: Colors.red[600]),
+                    style:
+                        new TextStyle(fontSize: 22.0, color: Colors.red[600]),
                   )),
                 ),
                 new Column(
@@ -51,28 +139,30 @@ class _EventsCardState extends State<EventsCard> {
                     Flexible(
                         child: Container(
                             child: new Text(
-                              "NTUOSS",
-                              overflow: TextOverflow.ellipsis,
-                              style: new TextStyle(fontSize: 18.0, color: Colors.black54),
-                            )))
+                      "NTUOSS",
+                      overflow: TextOverflow.ellipsis,
+                      style:
+                          new TextStyle(fontSize: 18.0, color: Colors.black54),
+                    )))
                   ],
                 ),
                 new Expanded(
                   child: Container(
                     child: new Align(
                       alignment: Alignment.centerRight,
-
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: new RaisedButton(
-                              child: new Text("More", style: new TextStyle(color: Colors.white, fontSize: 20.0)),
-                              shape: new RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(20.0 / 2))),
-                              color: Colors.blue,
-                              textColor: Colors.black87,
-                              onPressed: (){print("pressed");}),
-                        ),
-
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: new RaisedButton(
+                            child: new Text("More",
+                                style: new TextStyle(
+                                    color: Colors.white, fontSize: 20.0)),
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(20.0 / 2))),
+                            color: Colors.blue,
+                            textColor: Colors.black87,
+                            onPressed: _showDialog),
+                      ),
                     ),
                   ),
                 )
@@ -83,11 +173,11 @@ class _EventsCardState extends State<EventsCard> {
             padding: const EdgeInsets.all(8.0),
             child: new Container(
                 child: new Text(
-                  "This will be a small description of the thing so that people know what will happen",
-                  overflow: TextOverflow.clip,
-                  maxLines: 2,
-                  style: new TextStyle(fontSize: 18.0, color: Colors.black),
-                )),
+              "This will be a small description of the thing so that people know what will happen",
+              overflow: TextOverflow.clip,
+              maxLines: 2,
+              style: new TextStyle(fontSize: 18.0, color: Colors.black),
+            )),
           )
         ],
       )),
