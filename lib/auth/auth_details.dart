f41fae1168c4587b1b9aa9879ca89ca47773a317
HAN SIMENG <hans0035@e.ntu.edu.sg>
2019-02-05 23:42:20 +0800

    update

---
+++ lib/auth/auth_details.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+class AuthDetails extends StatelessWidget {
+
+  String password = " ";
+
+  String getSenderPassword(){
+    return "intuition2018";
+  }
+
+  String getSenderEmail(){
+    return "blinkceptionntu@gmail.com";
+  }
+
+  String getReportingEmail(){
+    return "atrikdas@gmail.com";
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return null;
+  }
+}
